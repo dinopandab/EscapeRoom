c6b50f57f1712dc0248756739b27983dcd77565d
Dinopandab <coronalizbeth567@gmail.com>
2021-12-03 13:23:22 -0500

    escape v2

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,5 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// contain 3 nodes (locations in the gui to click on )(door,small car with a battery pocket, dresser, safe,)
+//(1.go to dresser get skrewdriver but also dressor has a key 2. skrew driver is used to open car which has a code
+//radio buttons
+// methods
+//if statements
+// variablesd
+
+
+
 public class EscapeRoom {
+
+
     public static void main(String[] args) {
 
+
+
+
+
+
     }
+    public static void EscapeRoomGUI(){
+        JFrame frame;
+        JPanel panel;
+
+        frame= new JFrame("Escape Room");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        panel= new JPanel();
+
+
+
+
+
+        frame.setVisible(true);
+
+}
+
+
+
+
+
+
 }
